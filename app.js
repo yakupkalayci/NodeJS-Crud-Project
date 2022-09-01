5477bec3c4b357379eb289c3c935e495b2957f74
yakupkalayci <ykpklyc18@gmail.com>
2022-09-01 13:58:15 +0300

    template engine added

--- app.js
+++ app.js
@@ -1,12 +1,26 @@
 const express = require("express");
+const ejs = require("ejs");
 const path = require("path");
 
 const app = express();
 
+//MIDLEWARES
 app.use(express.static("public"));
 
+// TEMPLATE ENGINE
+app.set("view engine", "ejs");
+
+//ROUTES
 app.get("/", (req, res) => {
-    res.sendFile(path.resolve(__dirname, "temp/index.html"));
+    res.render("index");
+});
+
+app.get("/about", (req, res) => {
+    res.render("about");
+});
+
+app.get("/add", (req, res) => {
+    res.render("add");
 });
 
 const port = 3000;
