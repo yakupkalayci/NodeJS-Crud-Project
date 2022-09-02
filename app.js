f2e6623d3e75824a0c6f606660a6de879d892536
yakupkalayci <ykpklyc18@gmail.com>
2022-09-02 17:51:24 +0300

    update photo added

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const mongoose = require("mongoose");
 const fileUpload = require("express-fileupload");
+const methodOverride = require("method-override");
 const ejs = require("ejs");
 const path = require("path");
 const fs = require("fs");
@@ -16,6 +17,7 @@ app.use(express.static("public"));
 app.use(express.urlencoded({extended: true}));
 app.use(express.json());
 app.use(fileUpload());
+app.use(methodOverride("_method"));
 
 // TEMPLATE ENGINE
 app.set("view engine", "ejs");
@@ -67,6 +69,15 @@ app.post("/post", async (req, res) => {
         });
         res.redirect("/");
     });
+});
+
+app.put("/photos/:id", async(req, res) => {
+    const Photo = await photo.findById(req.params.id);
+    Photo.title = req.body.title;
+    Photo.description = req.body.description;
+    Photo.save();
+
+    res.redirect(`/photos/${req.params.id}`);
 });
 
 const port = 3000;
